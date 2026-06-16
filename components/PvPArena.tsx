58d22b42427c9de2e37e4d752b0e8ca3717261b4
Alchevora <tungkakxbok@gmail.com>
2026-06-16 15:25:03 +0700

    Create PvPArena.tsx

---
+++ components/PvPArena.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function PvPArena() {
+  return (
+    <section>
+      <h1>PvP Arena</h1>
+
+      <p>
+        Challenge other Wizards
+        and earn rewards.
+      </p>
+    </section>
+  );
+}
