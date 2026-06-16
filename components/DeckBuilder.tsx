ebc12e0b8d410d2bd4969b559029412d99eb0cde
Alchevora <tungkakxbok@gmail.com>
2026-06-16 15:24:50 +0700

    Create DeckBuilder.tsx

---
+++ components/DeckBuilder.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function DeckBuilder() {
+  return (
+    <div>
+      <h2>Deck Builder</h2>
+
+      <button>
+        Save Deck
+      </button>
+    </div>
+  );
+}
